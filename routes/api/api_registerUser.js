723a27bfe1f155fe64bfaa04905de2692a747085
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-01 20:04:29 -0400

    clean th ecomments in routs

--- routes/api/api_registerUser.js
+++ routes/api/api_registerUser.js
@@ -3,17 +3,12 @@ const express = require("express")
 const router = express.Router();
 const registerUser = require("../../controllers/registerUserController");
 
-// Matches with "/api/books"
+// Matches with "/api/register"
 router
   .route("/")
   .get(registerUser.findAll)
   .post(registerUser.create);
 
-// Matches with "/api/books/:id"
-// router
-//   .route("/:id")
-//   .get(registerUser.findById)
-//  // .put(registerUser.update)
-//   .delete(registerUser.remove);
+
 
 module.exports = router;
