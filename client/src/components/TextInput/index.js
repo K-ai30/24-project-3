ee1813124ad4e01a51f9ff00d914dfa1f5f0d3e0
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-14 19:37:34 -0400

    fix the Textinput conflict

--- client/src/components/TextInput/index.js
+++ client/src/components/TextInput/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import './style.css';
 
 const TextInput = props => {
-<<<<<<< HEAD
    
     if (props.HandleOnChangeText) {
         return (
@@ -21,15 +20,6 @@ const TextInput = props => {
     }
     
    
-=======
-    return (
-
-        <div className="form-group">
-            <label htmlFor={props.id}>{props.labelText}</label>
-            <input type="text" className="form-control" id={props.id} placeholder={props.placeholder}/>
-        </div>
-    )
->>>>>>> 7dd0ce13a11f6491fb361a97369d39e2c84a97a2
 }
 
 export default TextInput;
